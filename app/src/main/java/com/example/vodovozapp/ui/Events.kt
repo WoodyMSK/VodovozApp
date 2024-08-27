dfb0ad63b1344b602f5a2e2478917c21f3934ebf
WoodyMSK <woodyrzn@gmail.com>
2024-08-28 00:32:19 +0300

    Initial commit

--- app/src/main/java/com/example/vodovozapp/ui/Events.kt
+++ app/src/main/java/com/example/vodovozapp/ui/Events.kt
@@ -4,6 +4,9 @@ import com.example.vodovozapp.data.model.TovaryList
 
 sealed class Events {
 
-    data class ShowTovary(val tovary: TovaryList) : Events()
+    data class ShowTovary(
+        val tovary: TovaryList,
+        val selectedChipIndex: Int,
+    ) : Events()
     data object LoadingState : Events()
 }
